94e2c626d5006c1609be2950730a6acc0f19e200
as <dasdad>
2020-07-08 22:54:26 +0300

    ilk commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Oznur Avsan on 8.07.2020.
+//  Copyright © 2020 Oznur Avsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+    }
+
+
+}
+
